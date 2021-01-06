bf73d7be0122ba0458979c55fe883da1888dc3b1
Lâm Quang Khánh <lamquangkhanh2710@gmail.com>
2021-01-06 15:50:37 +0700

    update 2

---
+++ website/application/views/frontend/components/chinhsachvanchuyen/index.php
@@ -0,0 +1,42 @@
+<section id="content">
+	<div class="row wraper">
+		<div class="container">
+			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 list-menu pull-left">
+                    <?php $this->load->view('frontend/modules/category'); ?> 
+                </div>
+            </div>
+			<div class = "col-xs-12 col-sm-12 col-md-9 col-lg-9 product-content" id="list-content">
+				<div class="product-wrap" style="">
+                    <div class="wrap-2">
+                        <h2>Chính sách vận chuyển</h2>
+                        <div class="content">
+				<div class="des">
+					<p><strong><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Với phương châm Quyền lợi của khách hàng là quan trọng nhất, 24hStore Việt Nam xây dựng chính sách vận chuyển dành cho khách hàng như sau</span></strong></p>				</div>
+				<div class="main-content">
+					<p><strong><span style="font-family: arial, helvetica, sans-serif; font-size: small;">24hStore Việt Nam giao hàng tới tất cả các tỉnh thành thuộc lãnh thổ Việt Nam .</span></strong></p>
+<p><strong><span style="font-family: arial, helvetica, sans-serif; font-size: small;">1. PHẠM VI ÁP DỤNG :</span></strong><br><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Tất cả khách hàng mua hàng qua hotline hoặc tại website : <a href="http://24hStore.vn">http://24hStore.vn</a> hoặc Fanpage : <a href="https://FACEbook.com/24hStore.May.loc.khong.khi.Thuy.Si/" target="_blank">24hStore</a> có nhu cầu giao hàng tại nhà </span></p>
+<p><strong><span style="font-family: arial, helvetica, sans-serif; font-size: small;">2. HÌNH THỨC ÁP DỤNG:</span></strong></p>
+<ul>
+<li><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Sản phẩm &amp; phụ kiện bán nguyên giá được miễn phí vận chuyển toàn quốc. </span></li>
+<li><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Sản phẩm &amp; phụ kiện bán trong chương trình khuyến mại: Miễn phí ship nội thành Hà Nội và HCM. Với khách ở khu vực&nbsp;</span><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Ngoại thành và Ngoại tỉnh sẽ tính phí vận chuyển theo đơn vị chuyển phát nhanh.</span></li>
+<li><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Với sản phẩm đổi, bảo hành, bảo trì: Khách hàng chịu chi phí vận chuyển theo đơn vị chuyển phát của 24hStore.&nbsp;</span><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Chi phí này sẽ được 24hStore thông báo và xác nhận với quý khách trước khi quý khách tiến hành thanh toán và 24hStore tiến hành gửi hàng</span></li>
+</ul>
+<p><strong><span style="font-family: arial, helvetica, sans-serif; font-size: small;">3. THỜI GIAN GIAO HÀNG :</span></strong></p>
+<ul>
+<li><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Thời gian giao hàng sẽ từ <strong>1-5 ngày</strong> tùy vào khoảng cách vận chuyển.</span></li>
+<li><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Trong một số trường hợp khách quan 24hStore có thể giao hàng chậm trễ do những điều kiện bất khả kháng như thời tiết xấu, điều kiện giao thông không thuận lợi, xe hỏng hóc trên đường giao hàng, trục trặc trong quá trình xuất hàng.</span></li>
+<li><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Trong thời gian chờ đợi nhận hàng, Quý khách có bất cứ thắc mắc gì về thông tin vận chuyển xin vui lòng liên hệ hotline <strong>08.62632424 - 0981.242424</strong> của 24hStore để nhận trợ giúp.</span></li>
+</ul>
+<p><strong><span style="font-family: arial, helvetica, sans-serif; font-size: small;">4. TRÁCH NHIỆM VỚI HÀNG HÓA VẬN CHUYỂN .</span></strong></p>
+<p><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Dịch vụ vận chuyển của do 24hStore chỉ định sẽ chịu trách nhiệm với hàng hóa và các rủi ro như mất mát hoặc hư hại của hàng hóa trong suốt quá trình vận chuyển hàng từ 24hStore đến quý khách.</span><br><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Quý khách có trách nhiệm kiểm tra hàng hóa khi nhận hàng. Khi phát hiện hàng hóa bị hư hại, trầy xước, bể vỡ, móp méo, hoặc sai hàng hóa thì ký xác nhận tình trạng hàng hóa với Nhân viên giao nhận và thông báo ngay cho Bộ phận chăm sóc khách hàng theo số hotline của 24hStore.</span></p>
+<p><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Sau khi quý khách đã ký nhận hàng mà không ghi chú hoặc có ý kiến về hàng hóa. 24hStore không có trách nhiệm với những yêu cầu đổi trả vì hư hỏng, trầy xước, bể vỡ, móp méo, sai hàng hóa,… từ quý khách sau này.</span><br><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Nếu dịch vụ vận chuyển do quý khách chỉ định và lựa chọn thì quý khách sẽ chịu trách nhiệm với hàng hóa và các rủi ro như mất mát hoặc hư hại của hàng hóa trong suốt quá trình vận chuyển hàng từ 24hStore đến quý khách. Khách hàng sẽ chịu trách nhiệm cước phí và tổn thất liên quan.</span></p>
+<p><strong><span style="font-family: arial, helvetica, sans-serif; font-size: small;">5. CAM KẾT</span></strong></p>
+<p><span style="font-family: arial, helvetica, sans-serif; font-size: small;">24hStore cam kết giao hàng tới tất cả các tỉnh thành trong phạm vi toàn quốc.&nbsp;</span><span style="font-family: arial, helvetica, sans-serif; font-size: small;">Chúng tôi còn liên kết với các hãng vận chuyển hàng hóa chuyên nghiệp và uy tín trên thị trường …. đảm bảo cho hàng hóa được đảm bảo an toàn và đúng hẹn với quý khách hàng.</span></p>				</div>
+			</div>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</div>
+</section>
